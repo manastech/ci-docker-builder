751e1cc5aeb4bada63b92d90b60b9a8463cc931a
Julien Portalier <julien@portalier.com>
2023-01-26 12:38:31 +0100

    fixup! fix: disable shellcheck warning (BUILD_OPTS _must_ be splat)

--- build.sh
+++ build.sh
@@ -148,7 +148,7 @@ dockerBuildAndPush() {
   local IMAGE="${REPO}:${DOCKER_TAG}${TAG_SUFFIX}"
 
   echo "Building image ${IMAGE} from ${DIR}"
-  # shellcheck disable=SC2086 (BUILD_OPTS must be splat)
+  # shellcheck disable=SC2086
   docker build ${BUILD_OPTS} -t "${IMAGE}" "${DIR}"
 
   echo "Pushing ${IMAGE}"
